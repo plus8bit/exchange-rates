51df54fbf0a35148c16232309e35a8f78c020f6f
Nick <plus0bit@gmail.com>
2019-02-06 16:24:46 +0300

    Update exchange info

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(data => {
       const contents = `1 USD is equal to ${data.rates[currency]} ${currency}`
       document.querySelector('#result').innerHTML = contents;
+      const info = `Exchange rates published by the <a href="https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html">European Central Bank</a>.`;
+      document.querySelector('#info').innerHTML = info;
     })
     .catch(() => {
       document.querySelector('#result').innerHTML = 'There was an error.';
